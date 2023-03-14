620d8ecb231c07d6be4ad3efa1570b65d1d45eb3
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-03-14 15:41:31 +0530

    Update alive.py

--- Shikimori/modules/alive.py
+++ Shikimori/modules/alive.py
@@ -35,6 +35,7 @@ from Shikimori import ALIVE_MEDIA, UPDATE_CHANNEL, SUPPORT_CHAT, OWNER_USERNAME,
 from Shikimori.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext
+import time
 
 bot_name = f"{dispatcher.bot.first_name}"
 
@@ -56,9 +57,14 @@ def awake(update: Update, context: CallbackContext):
     
     first_name = update.effective_user.first_name
     user = message.from_user
+    bruh = message.reply_text("⚡")
+    time.sleep(1)
+    bruh.delete()
+    time.sleep(1)
+    
 
     TEXT = f"""
-    <b>Hey darling <a href="tg://user?id={user.id}">{first_name}</a>, I'm {bot_name} Robot.
+    Hey darling <a href="tg://user?id={user.id}">{first_name}</a>, I'm {bot_name} Robot.
 
 ⚪ I'm alive & Working Fine. Any doubt
 
