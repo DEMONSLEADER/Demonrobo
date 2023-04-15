99fb2e93ea8fae8a4d1592a4e71adc3a9ea1a1a3
Demonsleader <115692396+DEMONSLEADER@users.noreply.github.com>
2023-04-15 15:15:04 +0530

    Update __init__.py

--- Shikimori/__init__.py
+++ Shikimori/__init__.py
@@ -131,7 +131,7 @@ if ENV:
     API_HASH = os.environ.get("API_HASH", None)
     DB_URL = os.environ.get("DATABASE_URL")
     DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+#     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     ARQ_API = os.environ.get("ARQ_API_BASE_URL", None)
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", None).split()
