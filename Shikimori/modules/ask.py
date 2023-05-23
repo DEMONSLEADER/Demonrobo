4964b340dabe88cf94663cb7efce5f43a463ee98
Demonsleader <115692396+DEMONSLEADER@users.noreply.github.com>
2023-05-23 08:30:03 +0530

    Update ask.py

--- Shikimori/modules/ask.py
+++ Shikimori/modules/ask.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-import openai
+import Shikimori 
 from telegram import Update
 from telegram.ext import Application, ContextTypes, Defaults
 
